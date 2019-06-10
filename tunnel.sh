1b5dd661e36c52ac956fad0a0c4566b99b060045
Karl Pokus <karl.pokus@gmail.com>
2019-06-10 22:07:57 +0200

    move tunnel to rootdir

--- tunnel.sh
+++ tunnel.sh
@@ -3,6 +3,9 @@
 # USAGE
 # $ ./tunnel.sh start|stop
 
+# starts an ssh tunnel to $HOST
+# stops the tunnel on SIGINT if being run by a daemon
+
 if test $# -eq 0; then
   echo missing arg
   exit 2
@@ -12,18 +15,33 @@ CMD=$1
 SOCKET="pets-mongo-socket"
 HOST="pets-mongo"
 
-if test $CMD = "start"; then
+function start_sock() {
   if test -S $SOCKET; then
     echo "already running"
   else
     echo "starting socket"
     ssh -f -N -M -S $SOCKET -L 4321:localhost:27017 $HOST
   fi
-elif test $CMD = "stop"; then
+}
+
+function stop_sock {
   if test -S $SOCKET; then
     echo "stopping socket"
-    ssh -S $SOCKET -O exit $HOST
+    ssh -S $SOCKET -O exit $HOST > /dev/null 2>&1
   else
     echo "no socket to stop"
   fi
+}
+
+if test $CMD = "start"; then
+  start_sock
+elif test $CMD = "stop"; then
+  stop_sock
+fi
+
+if ! test -t 1; then
+  trap 'stop_sock; exit 0' SIGINT
+  while true; do
+    :
+  done
 fi

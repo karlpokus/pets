d943fb158d8d1cb00e9039d53c309c84be406162
Karl Pokus <karl.pokus@gmail.com>
2019-06-17 09:42:16 +0200

    add os arg to pets build script

--- src/pets/build.sh
+++ src/pets/build.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+# build the pets binary for the specified os
+
+# USAGE
+# ./build.sh <os>
+
+# set cwd to wherever this script is located
+cd "$(dirname "$0")"
+
+OS=$1
 VERSION=`cat version`
-go build -ldflags "-X pets.Version=$VERSION" -o bin/pets ./cmd/pets
+
+if test -z $OS; then
+  echo "missing arg"
+  exit 1
+fi
+
+if test $OS != "darwin" && test $OS != "linux"; then
+  echo "unknown os"
+  exit 1
+fi
+
+echo "building pets $VERSION for $OS"
+GOOS=$OS GOARCH=amd64 go build -ldflags "-X pets.Version=$VERSION" -o bin/$OS/pets ./cmd/pets
+echo done

48ac367c804dbad3b9a62f7afaf9f32ac489eea8
Karl Pokus <karl.pokus@gmail.com>
2019-06-24 21:28:07 +0200

    require passing tests to build pets

--- src/pets/build.sh
+++ src/pets/build.sh
@@ -21,6 +21,13 @@ if test $OS != "darwin" && test $OS != "linux"; then
   exit 1
 fi
 
+go test
+TEST_RESULT=`echo $?`
+if test $TEST_RESULT -ne 0; then
+  echo "tests failed. Exiting"
+  exit $TEST_RESULT
+fi
+
 echo "building pets $VERSION for $OS"
 GOOS=$OS GOARCH=amd64 go build -ldflags "-X pets.Version=$VERSION" -o bin/$OS/pets ./cmd/pets
 echo done

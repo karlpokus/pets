c5a7df75cf450916f640146717e7ac75c3fd18be
Karl Pokus <karl.pokus@gmail.com>
2019-10-18 16:32:15 +0200

    update build script for pets service

--- src/pets/build.sh
+++ src/pets/build.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# build the pets binary for the specified os
-
-# USAGE
-# ./build.sh <os>
-
-# set cwd to wherever this script is located
-cd "$(dirname "$0")"
-
-OS=$1
-VERSION=`cat version`
-
-if test -z $OS; then
-  echo "missing arg"
-  exit 1
-fi
-
-if test $OS != "darwin" && test $OS != "linux"; then
-  echo "unknown os"
-  exit 1
-fi
-
 go test
 TEST_RESULT=`echo $?`
 if test $TEST_RESULT -ne 0; then
@@ -28,6 +7,6 @@ if test $TEST_RESULT -ne 0; then
   exit $TEST_RESULT
 fi
 
-echo "building pets $VERSION for $OS"
-GOOS=$OS GOARCH=amd64 go build -ldflags "-X pets.Version=$VERSION" -o bin/$OS/pets ./cmd/pets
-echo done
+VERSION=`cat version`
+
+docker build -t pokus2000/pets-service:$VERSION .
